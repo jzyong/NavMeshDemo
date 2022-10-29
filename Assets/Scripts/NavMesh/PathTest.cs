3b3e4f1ab24162e895d30399e69e1671475b792d
jzyong <359135103@qq.com>
2022-10-29 23:49:37 +0800

    player and animator

--- Assets/Scripts/NavMesh/PathTest.cs
+++ Assets/Scripts/NavMesh/PathTest.cs
@@ -10,15 +10,20 @@ public class PathTest : MonoBehaviour
 {
     NavMeshAgent agent;
 
-    public float distanceUp = 15f; //相机与目标的竖直高度参数
-    public float distanceAway = 20f; //相机与目标的水平距离参数
+    public float distanceUp = 10f; //相机与目标的竖直高度参数
+    public float distanceAway = 15f; //相机与目标的水平距离参数
     public float smooth = 2f; //位置平滑移动插值参数值
     public float camDepthSmooth = 20f;
+    private Animator _animator;
 
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+        // _animator.SetTrigger("HumanoidIdle");
+        _animator.SetFloat("speed",0);
+
     }
 
     void Update()
@@ -31,14 +36,22 @@ public class PathTest : MonoBehaviour
             {
                 Debug.Log($"点击位置：{hit.point}");
                 agent.destination = hit.point;
+                _animator.SetFloat("speed",agent.speed);
             }
         }
+        
 
         // 鼠标轴控制相机的远近
         if ((Input.mouseScrollDelta.y < 0 && Camera.main.fieldOfView >= 3) ||
             Input.mouseScrollDelta.y > 0 && Camera.main.fieldOfView <= 80)
         {
             Camera.main.fieldOfView += Input.mouseScrollDelta.y * camDepthSmooth * Time.deltaTime;
+        }
+        
+        // 距离判断
+        if (Vector3.Distance(agent.destination, transform.position) < 1)
+        {
+            _animator.SetFloat("speed",0f);
         }
     }
 
